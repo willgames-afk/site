149f6443f0681ce126c91d7c52b1323b8e186392
Will Kam <69923277+willgames-afk@users.noreply.github.com>
2022-10-12 18:37:01 +0000

    Begin migrating to template system

--- public/TGL/main.js
+++ public/TGL/main.js
@@ -1,10 +1,12 @@
 import { TextIO } from "../text-input-engine/main.js";
 import { lex, parse } from "./modules/parser.js"
 
+console.log("HI!")
+
 new TextIO(
 (input) => {
 	//Get rid of loading box
-	document.getElementById("loadingMessage").style.display = "none";
+	document.getElementsByClassName("loadBox")[0].style.display = "none";
 	return JSON.stringify(parse(lex(input))).replace(/},/g, '},\n')
 	
 }, { runImmediate: true, runAuto:true, defaultInputFile: "./testProgram.tgl", controls: true})
